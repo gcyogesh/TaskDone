ed23e104802aed2cdd4d3d568bcc9ebb9f15a3f4
gcyogesh <gcyogesh962@gmail.com>
2024-05-01 22:10:55 +0545

    Deployment

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faApple, faFacebook, faGoogle, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import {   faHome, faContactBook, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { faApple, faFacebook, faGoogle, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
   return (
